602a1d62f3b3a570fdc6ec9fd6b419ae44d304b6
Aaron Koeppel <akoeppel212@gmail.com>
2015-05-27 15:47:09 -0700

    added glm to perspective 113

--- src/Camera.hpp
+++ src/Camera.hpp
@@ -110,7 +110,7 @@ void Camera::update() {
     }
     else if (mode == SPLASH_CAM) {
         position = glm::vec3(174.789032, 100, 271.474487);
-	glm::vec3 dir = normalize(glm::vec3(198.505310, 30, 187.099915) - position);
+	glm::vec3 dir = glm::normalize(glm::vec3(198.505310, 30, 187.099915) - position);
 	rotation = glm::rotation(glm::vec3(0, 0, -1), dir);
     }
 }
